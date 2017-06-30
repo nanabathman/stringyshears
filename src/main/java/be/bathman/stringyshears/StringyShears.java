4001a9e6a4940f22b07df59368750b4c491c5a8e
Awe23123 <tszfungwonghk@gmail.com>
2017-07-01 04:50:16 +0800

    Cleanup
    -Merged two files
    -Renamed file name and method name
    -Removed useless code

--- src/main/java/be/bathman/stringyshears/StringyShears.java
+++ src/main/java/be/bathman/stringyshears/StringyShears.java
@@ -1,15 +1,22 @@
 package be.bathman.stringyshears;
 
-import be.bathman.stringyshears.event.strinyShearsEvent;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.Random;
 
 @Mod(modid = StringyShears.MODID, version = StringyShears.VERSION,name = StringyShears.NAME)
 public class StringyShears
 {
+	private static final Random rand = new Random();
+	
     public static final String MODID = "stringyshears";
     public static final String VERSION = "1.0";
     public static final String NAME = "Stringy Shears";
@@ -17,6 +24,15 @@ public class StringyShears
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        MinecraftForge.EVENT_BUS.register(new strinyShearsEvent());
+        MinecraftForge.EVENT_BUS.register(this);
     }
+
+    @SubscribeEvent
+    public void onPlayerHarvest(BlockEvent.HarvestDropsEvent event) {
+        if (event.getHarvester() != null && event.getState().getBlock() == Blocks.WOOL && event.getHarvester().getHeldItemMainhand().getItem() == Items.SHEARS) {
+            event.getDrops().remove(0);
+            event.getDrops().add(new ItemStack(Items.STRING, rand.nextInt(4) + 2));
+        }
+    }
+	
 }
